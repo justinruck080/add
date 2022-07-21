edcc9f23dbb6957ff35f3bed082537a8c01403ad
justinruck080 <80236509+justinruck080@users.noreply.github.com>
2022-07-21 05:56:05 -0400

    addddd

---
+++ try.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex-archive-keyring.gpg >/dev/null
+
+echo deb [signed-by=/usr/share/keyrings/plex-archive-keyring.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+
+sudo apt-get update
+
